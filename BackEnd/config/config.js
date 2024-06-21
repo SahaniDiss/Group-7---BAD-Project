b9eb6a5f47be64b58d9f05e5319cc6d1d5df94d5
SahaniDiss <shdissanayake23@gmail.com>
2024-06-21 23:39:51 +0530

    Initialising BackEnd

---
+++ BackEnd/config/config.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+    development: {
+      username: 'root',
+      password: 'your_password',
+      database: 'restaurant_db',
+      host: '127.0.0.1',
+      dialect: 'mysql'
+    },
+    production: {
+      username: 'root',
+      password: 'your_password',
+      database: 'restaurant_db',
+      host: '127.0.0.1',
+      dialect: 'mysql'
+    }
+  };
+  
